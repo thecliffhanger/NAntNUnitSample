c3abdb429391a2212e97dea1955a630403a1a660
Teja <raviteja.prabhala@gmail.com>
2014-12-16 15:15:10 -0600

    Sample NAnt and NUnit Project

---
+++ SampleWebApp/src/SampleUnitTest/UnitTest1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace SampleUnitTest
+{
+    [TestFixture]
+    public class UnitTest1
+    {
+        [Test]
+        public void TestMethod1()
+        {
+            Assert.IsTrue(true);
+        }
+
+        [Test]
+        public void TestMethod2()
+        {
+            Assert.IsTrue(true);
+        }
+
+        [Test, ExpectedException]
+        public void TestMethod3()
+        {
+            throw new Exception("Exception");
+        }
+
+        [Test, Ignore]
+        public void TestMethod4()
+        {
+            throw new Exception("Exception");
+        }
+    }
+}
